537e886d45af83d42e212202d4bf03b00200e6d6
Conrado <conrado-ss@tutanota.com>
2021-02-13 21:28:41 -0300

    Minor improvements in class ApiManager.

--- resources/io/api_manager.py
+++ resources/io/api_manager.py
@@ -5,48 +5,49 @@ import hashlib
 import yaml
 import re
 import os
-import os.path as path
-from collections import defaultdict
+import datetime
+import dateutil.tz as tz
 
-def rename_keys(kval_pair, key='name'):
-    """Utility function for changing *key* on (key, value) pairs."""
-
-    old_key, contents = kval_pair 
-    if key in contents.keys():
-        new_key = contents[key]
-        contents.pop(key)
-    else:
-        new_key = old_key
-
-    return new_key, contents
-
-class apiManager():
+class ApiManager():
     def __init__(self, path, base_dir, monitor_api=False):
         if os.path.isdir(base_dir):
             self.base_directory = os.path.abspath(base_dir)
         self._monitor_api = monitor_api
         self._parse_config_file(path)
+        self.last = '' 
 
     def fetch(self, force=False):
 
         if self._check_api_status() and not force:
+            print('Last time API was accessed: %s'%self.last)
             print("Current files are up to date.\nTo force process start set *force* True.")
-        else:
 
-            print("API has been updated. Starting process...")
+        else:
+            print(f"Fetching {len(self.endpoints)} endpoints from {self.name.upper()}")
+            print(f"[{current_date('%b %d. %H:%M', tz=tz.tzlocal())}] Starting process.\n")
+
             # building directory tree for incoming files
             self._make_dirs()
 
             # fetching/storaging files
-            for endpoint_name in self.endpoints:
+            for i, endpoint_name in enumerate(self.endpoints, 1):
+                # composing url
+                print(f'[{i}/{len(self.endpoints)}] Endpoint: {endpoint_name.upper()}', end=' -> ')
                 endpoint_api_string = self._get_api_string(endpoint_name)
                 url = self.api + endpoint_api_string
+
+                # requesting
+                print('Requesting API', end='... ')
+                response = requests.get(url)
+
+                # writing content
                 filename = self._get_filename(endpoint_name)
                 file_out = os.path.join(self.base_directory, endpoint_name, filename)
-                response = requests.get(url)
                 with open(file_out, 'wb') as out:
+                    print('Writing content\n')
                     out.write(response.content)
-            
+
+            self.last = current_date()
             print('Fetching files concluded.')
 
     def _parse_config_file(self, path):
@@ -76,11 +77,11 @@ class apiManager():
     def _check_api_status(self):
         check = True
         if self._monitor_api:
-            if '_last' not in dir(self):
-                self._last = self._make_hash()
+            if '_sha1' not in dir(self):
+                self._sha1 = self._make_hash()
                 check = False
             else:
-                check = self._last == self._make_hash()
+                check = self._sha1 == self._make_hash()
         return check
 
     def _make_hash(self):
@@ -106,20 +107,38 @@ class apiManager():
         api_string = self._get_api_string(endpoint_name)
         return re.match(r".*/(.*)$", api_string).group(1)
  
+def rename_keys(kval_pair, key='name'):
+    """Utility function for changing *key* on (key, value) pairs."""
+
+    old_key, contents = kval_pair 
+    if key in contents.keys():
+        new_key = contents[key]
+        contents.pop(key)
+    else:
+        new_key = old_key
+
+    return new_key, contents
+
+def current_date(strftime='%A, %Y-%m-%d %T %Z', tz=tz.UTC):
+    """Utility function to return formated current datetime."""
+
+    now = datetime.datetime.now()
+    return now.astimezone(tz).strftime(strftime)
+
 
 # code for testing purposes
-if __name__ == '__main__':
-    path = './.config/covid_api.yml'
-    covid_api = apiManager(path=path, base_dir='./.cache', monitor_api=True)
-    print(covid_api.name)
-    print(covid_api.api)
-    print(covid_api.status['keys'])
-    print(len(covid_api.endpoints))
-    print(covid_api.base_directory)
+# if __name__ == '__main__':
+#     path = './.config/covid_api.yml'
+#     covid_api = apiManager(path=path, base_dir='./.cache', monitor_api=True)
+#     print(covid_api.name)
+#     print(covid_api.api)
+#     print(covid_api.status['keys'])
+#     print(len(covid_api.endpoints))
+#     print(covid_api.base_directory)
 
-    print('Test #1')
-    covid_api.fetch() # Testing 1st use of api
-    print('Test #2')
-    covid_api.fetch() # Testing calling API (with updated status)
-    print('Test #3')
-    covid_api.fetch(force=True) # Testing Force-calling API
+#     print('Test #1')
+#     covid_api.fetch() # Testing 1st use of api
+#     print('Test #2')
+#     covid_api.fetch() # Testing calling API (with updated status)
+#     print('Test #3')
+#     covid_api.fetch(force=True) # Testing Force-calling API
